906fcff008d17bf31ed0d5ceb312b29d016aa31c
Jhon Eduard Bocanegra <jhon.bocanegra26@gmail.com>
2022-09-29 12:25:51 -0500

    [ADD] registerpage design

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,5 +1,59 @@
+import { Link as RouterLink } from 'react-router-dom'
+import { Button, Grid, Link, TextField } from '@mui/material'
+import { AuthLayout } from '../layout/AuthLayout'
+
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <AuthLayout title='Create an account'>
+      <form>
+        <Grid container spacing={0} sx={{ mt: 2 }}>
+          <Grid item xs={12} sx={{ mb: 1 }}>
+            <TextField
+              fullWidth
+              label="Full name"
+              type="text"
+              placeholder="Your name"
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ mb: 1 }}>
+            <TextField
+              fullWidth
+              label="Email"
+              type="email"
+              placeholder="youremail@mail.com"
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ mb: 1 }}>
+            <TextField
+              fullWidth
+              label="Password"
+              type="password"
+              placeholder="*********"
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ mb: 1 }}>
+            <TextField
+              fullWidth
+              label="Confirm password"
+              type="password"
+              placeholder="*********"
+            />
+          </Grid>
+
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12}>
+              <Button variant="contained" fullWidth>Sign in</Button>
+            </Grid>
+          </Grid>
+
+          <Grid container justifyContent="end">
+            <Link component={RouterLink} to="/auth/login">
+              Do you have an account? Log in
+            </Link>
+          </Grid>
+
+        </Grid>
+      </form>
+    </AuthLayout>
   )
 }
